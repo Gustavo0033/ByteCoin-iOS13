0eac8f69becc39d6d334d21c553e254f3f4a84d0
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2024-09-10 19:54:40 -0300

    Done!

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -8,13 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitCoinLabel: UILabel!
+    @IBOutlet weak var currentPicker: UIPickerView!
+    
+    var coinManager = CoinManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        coinManager.delegate = self
+        currentPicker.dataSource = self
+        currentPicker.delegate = self
     }
+    
+
+    
+
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        coinManager.getCoinPrice(for: selectedCurrency)
+    }
+}
 
 
+//MARK: - Update de price and currency from the country choosed
+
+extension ViewController: CoinManagerDelegate{
+    
+    func didFailWithError(error: any Error) {
+        print(error)
+    }
+    
+    func didUpdatePrice(price: String, currency: String) {
+        
+        //Remember that we need to get hold of the main thread to update the UI, otherwise our app will crash if we
+        //try to do this from a background thread (URLSession works in the background).
+        DispatchQueue.main.async {
+            self.bitCoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
 }
 
---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Gustavo Mendonca on 10/09/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+struct CoinData: Decodable{
+
+    
+    let rate: Double
+}
